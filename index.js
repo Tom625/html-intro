b25212e47b98cb3f8dee1de4e4600802c344642d
Thomas George <leo.club625@gmail.com>
2020-02-07 11:43:41 +0530

    Added option for public static files

--- index.js
+++ index.js
@@ -1,7 +1,15 @@
 const express = require('express');
+const bodyParser = require('body-parser');
+var path = require('path');
+
 
 const app = express();
 const port = 3000;
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static(path.join(__dirname, 'public')));
 
 app.get('/', (req, res)=>{
     res.send('Html Practice')
